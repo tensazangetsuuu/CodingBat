3e4d7e5a78dc20245dab2b2ee951fa4915918573
Ramis Almazbekov <ra.almazbekov@gmail.com>
2026-01-18 18:22:48 +0600

    the 10 commits of string1 tasks
    string1: solved helloName
    
    string1: solved makeAbba
    
    string1: solved makeTags
    
    string1: solved makeOutWord
    
    string1: solved extraEnd
    
    string1: solved firstTwo
    
    string1: solved firstHalf
    
    string1: solved withoutEnd
    
    string1: solved comboString
    
    string1: solved nonStart

---
+++ src/string1/HelloName.java
@@ -0,0 +1,8 @@
+package string1;
+
+public class HelloName {
+
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
---
+++ src/string1/MakeTags.java
@@ -0,0 +1,7 @@
+package string1;
+
+public class MakeTags {
+    public String makeTags(String tag, String word) {
+        return "<"+tag+">" + word +"</"+tag+">";
+    }
+}
